aaa75f93230f10828c6e5abc51ea69ce705442ee
eliasfernandescout <eliasfernandescout@gmail.com>
2023-03-13 15:53:11 -0300

    test: update tests

--- test/Board.test.ts
+++ test/Board.test.ts
@@ -1,6 +1,8 @@
+import { Column } from "pg-promise";
 import Board from "../src/Board";
 
 test("Deve criar um quadro", function(){
     const board = new Board("Projeto 1");
     expect(board.name).toBe("Projeto 1");
-})
+});
+
--- test/Column.test.ts
+++ test/Column.test.ts
@@ -4,5 +4,8 @@ test("Deve criar uma coluna", function(){
     const column = new Column("Coluna A", true);
     expect(column.name).toBe("Coluna A");
     expect(column.hasEstimative).toBeTruthy();
+});
 
-})
+test("Deve deve criar uma coluna sem nome", function(){
+    expect(()=>new Column("", true)).toThrow(new Error("Name is required"));
+});
