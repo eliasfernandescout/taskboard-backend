ab0f030bce26370e89463b19327cf610ffab7fd3
eliasfernandescout <eliasfernandescout@gmail.com>
2023-03-16 12:03:12 -0300

    feat: add cardRepository in server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import pgPromise from "pg-promise";
 import PgPromiseConnection from "./infra/database/PgPromiseConnection";
 import BoardRepositoryDatabase from "./infra/repository/BoardRepositoryDatabase";
+import CardRepositoryDatabase from "./infra/repository/CardRepositoryDatabase";
 import ColumnRepositoryDatabase from "./infra/repository/ColumnRepositoryDatabase";
 import BoardService from "./service/BoardService";
 import CardService from "./service/CardService";
@@ -11,6 +12,7 @@ const app = express();
 const connection = new PgPromiseConnection();
 const boardRepository = new BoardRepositoryDatabase(connection);
 const columnRepository = new ColumnRepositoryDatabase(connection);
+const cardRepository = new CardRepositoryDatabase(connection);
 
 app.get("/boards", async function(request, response){
     const boardService = new BoardService(boardRepository);
@@ -25,7 +27,7 @@ app.get("/boards/:idBoard/columns", async function(request, response){
 });
 
 app.get("/boards/:idBoard/columns/:idColumn/cards", async function(request, response){
-    const cardService = new CardService();
+    const cardService = new CardService(cardRepository);
    const cards = await cardService.getCards(parseInt(request.params.idColumn));
    response.json(cards);
     
