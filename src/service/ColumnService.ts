4616a92dcab6d1df7e290f3fcf5fa2580c25c94c
eliasfernandescout <eliasfernandescout@gmail.com>
2023-03-16 10:37:23 -0300

    feat: new method to get columns

--- src/service/ColumnService.ts
+++ src/service/ColumnService.ts
@@ -1,20 +1,14 @@
-import pgPromise from "pg-promise";
-import Column from "../domain/Column";
+import ColumnRepository from "../domain/repository/ColumnRepository"
 
 export default class ColumnService {
+    readonly columnRepository: ColumnRepository;
     
-    constructor(){
-
+    constructor(columnRepository: ColumnRepository){
+        this.columnRepository = columnRepository;
     };
 
     async getColumns(idBoard: number){
-        const connection = pgPromise()("postgres://postgres:postgres@127.0.0.1:5432/app"); 
-        const columnsData = await connection.query("select name, has_estimative from taskboard.column where id_board = $1", [idBoard]);
-        const columns: Column[] = [];
-        for(const columnData of columnsData){
-        columns.push(new Column(columnData.name, columnData.has_estimative));
-        };
-        await connection.$pool.end();
+        const columns = await this.columnRepository.findAllByIdBoard(idBoard);
         return columns;
     };
 };
