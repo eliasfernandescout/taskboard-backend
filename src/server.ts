bec194ffd9f60e3546b21ca58e181b7822e9c07e
eliasfernandescout <eliasfernandescout@gmail.com>
2023-03-16 10:34:31 -0300

    refactor: server just sending and receiving requests

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,25 @@
 import express from "express";
 import pgPromise from "pg-promise";
 import PgPromiseConnection from "./infra/database/PgPromiseConnection";
+import BoardRepositoryDatabase from "./infra/repository/BoardRepositoryDatabase";
+import ColumnRepositoryDatabase from "./infra/repository/ColumnRepositoryDatabase";
 import BoardService from "./service/BoardService";
 import CardService from "./service/CardService";
 import ColumnService from "./service/ColumnService";
 
 const app = express();
 const connection = new PgPromiseConnection();
+const boardRepository = new BoardRepositoryDatabase(connection);
+const columnRepository = new ColumnRepositoryDatabase(connection);
 
 app.get("/boards", async function(request, response){
-    const boardService = new BoardService();
+    const boardService = new BoardService(boardRepository);
     const boards = await boardService.getBoards();
     response.json(boards);
 });
 
 app.get("/boards/:idBoard/columns", async function(request, response){
-   const columnService = new ColumnService();
+   const columnService = new ColumnService(columnRepository);
    const columns = await columnService.getColumns(parseInt(request.params.idBoard));
    response.json(columns);
 });
