9d46179621b9e575567cf329e3b7ce1f23eab3c8
eliasfernandescout <eliasfernandescout@gmail.com>
2023-03-16 10:36:15 -0300

    feat: new method to get boards

--- src/service/BoardService.ts
+++ src/service/BoardService.ts
@@ -1,29 +1,13 @@
-import pgPromise from "pg-promise";
-import Board from "../domain/Board";
+import BoardRepository from "../domain/repository/BordRepository";
 
 export default class BoardService{
+    readonly boardRepository: BoardRepository;
 
-    constructor(){
-
+    constructor(boardRepository: BoardRepository){
+        this.boardRepository = boardRepository;
     };
-
     async getBoards(){
-        
-        const boardsData = await connection.query("select name from taskboard.board", []);
-        
-        const boards: Board[] = [];
-        for(const boardData of boardsData){
-            const cardsData = await connection.query("select * from taskboard.card join taskboard.column using (id_column) where id_board = $1", [boardData.id_board])
-            let estimative = 0;
-
-            for(const cardData of cardsData){
-                estimative += cardData.estimative;
-            };
-            const board = new Board(boardData.name)
-            board.estimative = estimative;
-            boards.push(board);
-        };
-        await connection.$pool.end();
+        const boards = await this.boardRepository.findAll();        
         return boards;
     };
 };
