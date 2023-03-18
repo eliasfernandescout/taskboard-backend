a28d3a491a7c25873d53a2d29b644ccc93e5b7ea
eliasfernandescout <eliasfernandescout@gmail.com>
2023-03-17 23:31:14 -0300

    feat: update server

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,12 @@
-import express from "express";
-import pgPromise from "pg-promise";
+import BoardController from "./infra/controller/BoardController";
+import CardController from "./infra/controller/CardController";
+import ColumnController from "./infra/controller/ColumnController";
 import PgPromiseConnection from "./infra/database/PgPromiseConnection";
-import BoardRepositoryDatabase from "./infra/repository/BoardRepositoryDatabase";
-import CardRepositoryDatabase from "./infra/repository/CardRepositoryDatabase";
-import ColumnRepositoryDatabase from "./infra/repository/ColumnRepositoryDatabase";
-import BoardService from "./service/BoardService";
-import CardService from "./service/CardService";
-import ColumnService from "./service/ColumnService";
+import ExpressAdapter from "./infra/http/ExpressAdapter";
 
-const app = express();
 const connection = new PgPromiseConnection();
-const boardRepository = new BoardRepositoryDatabase(connection);
-const columnRepository = new ColumnRepositoryDatabase(connection);
-const cardRepository = new CardRepositoryDatabase(connection);
-
-app.get("/boards", async function(request, response){
-    const boardService = new BoardService(boardRepository);
-    const boards = await boardService.getBoards();
-    response.json(boards);
-});
-
-app.get("/boards/:idBoard/columns", async function(request, response){
-   const columnService = new ColumnService(columnRepository);
-   const columns = await columnService.getColumns(parseInt(request.params.idBoard));
-   response.json(columns);
-});
-
-app.get("/boards/:idBoard/columns/:idColumn/cards", async function(request, response){
-    const cardService = new CardService(cardRepository);
-   const cards = await cardService.getCards(parseInt(request.params.idColumn));
-   response.json(cards);
-    
-});
-
-app.listen(3000);
+const httpServer = new ExpressAdapter();
+new BoardController(httpServer, connection);
+new ColumnController(httpServer, connection);
+new CardController(httpServer, connection);
+httpServer.listen(3000);
