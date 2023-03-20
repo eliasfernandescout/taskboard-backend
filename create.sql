3c7caf5768021b600c567b1e037567d93068e7e0
eliasfernandescout <eliasfernandescout@gmail.com>
2023-03-20 13:17:48 -0300

    chore: update sql

--- create.sql
+++ create.sql
@@ -1,6 +1,6 @@
-drop table taskboard.card;
-drop table taskboard.column;
-drop table taskboard.board
+drop table if exists taskboard.card;
+drop table if exists taskboard.column;
+drop table if exists taskboard.board
 
 
 create table taskboard.board(
@@ -23,12 +23,12 @@ create table taskboard.card(
 );
 
 
-insert into taskboard.board (id_board, name) values (1, 'Projeto 1');
+insert into taskboard.board (name) values ('Projeto 1');
 
-insert into taskboard.column (id_column, id_board, name, has_estimative) values (1, 1, 'Coluna A', true);
-insert into taskboard.column (id_column, id_board, name, has_estimative) values (2, 1, 'Coluna B', true);
-insert into taskboard.column (id_column, id_board, name, has_estimative) values (3, 1, 'Coluna C', true);
+insert into taskboard.column (id_board, name, has_estimative) values (1, 'Coluna A', true);
+insert into taskboard.column (id_board, name, has_estimative) values (1, 'Coluna B', true);
+insert into taskboard.column (id_board, name, has_estimative) values (1, 'Coluna C', true);
 
-insert into taskboard.card (id_card, id_column, title, estimative ) values (1, 1, 'Atividade 1', 3);
-insert into taskboard.card (id_card, id_column, title, estimative ) values (2, 1, 'Atividade 2', 2);
-insert into taskboard.card (id_card, id_column, title, estimative ) values (3, 1, 'Atividade 3', 1);
+insert into taskboard.card (id_column, title, estimative ) values (1, 'Atividade 1', 3);
+insert into taskboard.card (id_column, title, estimative ) values (1, 'Atividade 2', 2);
+insert into taskboard.card (id_column, title, estimative ) values (1, 'Atividade 3', 1);
